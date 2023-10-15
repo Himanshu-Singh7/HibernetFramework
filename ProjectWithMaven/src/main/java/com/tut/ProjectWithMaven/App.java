8f703ac539926059e940ffd14179468b11c811e0
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-16 00:48:26 +0530

    Add files via upload

--- ProjectWithMaven/src/main/java/com/tut/ProjectWithMaven/App.java
+++ ProjectWithMaven/src/main/java/com/tut/ProjectWithMaven/App.java
@@ -1,13 +1,17 @@
 package com.tut.ProjectWithMaven;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Project started..." );
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
     }
 }
