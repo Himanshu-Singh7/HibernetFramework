340d931bd8786435507b508beeb2baecb5deaca9
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-16 18:09:22 +0530

    Add files via upload

--- ProjectWithMaven/src/main/java/com/tut/ProjectWithMaven/App.java
+++ ProjectWithMaven/src/main/java/com/tut/ProjectWithMaven/App.java
@@ -1,6 +1,8 @@
 package com.tut.ProjectWithMaven;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class App 
@@ -10,8 +12,22 @@ public class App
         System.out.println( "Project started..." );
         Configuration cfg = new Configuration();
         cfg.configure("hibernate.cfg.xml");
+        cfg.setProperty("hibernate.current_session_context_class", "thread");
         SessionFactory factory = cfg.buildSessionFactory();
-        System.out.println(factory);
-        System.out.println(factory.isClosed());
+//        System.out.println(factory);
+//        System.out.println(factory.isClosed());
+       // Student Object 
+        Student st = new Student();
+        st.setId(103);
+        st.setName("Rahul");
+        st.setCity("Varansi");
+        
+       System.out.println(st);
+       Session session = factory.getCurrentSession();
+       Transaction tx =session.beginTransaction();
+       session.save(st);
+       tx.commit();
+       session.close();
+        
     }
 }
