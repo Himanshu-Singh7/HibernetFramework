be1d871437a47541af377464b56901c9127da024
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-16 19:23:43 +0530

    Add files via upload

--- ProjectWithMaven/src/main/java/com/tut/ProjectWithMaven/App.java
+++ ProjectWithMaven/src/main/java/com/tut/ProjectWithMaven/App.java
@@ -1,5 +1,9 @@
 package com.tut.ProjectWithMaven;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -7,8 +11,7 @@ import org.hibernate.cfg.Configuration;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args )throws IOException{
         System.out.println( "Project started..." );
         Configuration cfg = new Configuration();
         cfg.configure("hibernate.cfg.xml");
@@ -16,18 +19,36 @@ public class App
         SessionFactory factory = cfg.buildSessionFactory();
 //        System.out.println(factory);
 //        System.out.println(factory.isClosed());
-       // Student Object 
+       
+        //creating object of student 
         Student st = new Student();
         st.setId(103);
         st.setName("Rahul");
         st.setCity("Varansi");
         
+        //Creating object of Address
+        Address address = new Address();
+  
+        address.setStreet("Street1");
+        address.setCity("Banglore");
+        address.setOpen(true);
+        address.setAddedDate(new Date());
+        address.setX(1234.5432);
+        //Reading image
+        
+        FileInputStream file = new FileInputStream("src/main/java/image.png.jpeg");
+        byte[] data = new byte[file.available()];
+        file.read(data);
+        address.setImage(data);
+        
        System.out.println(st);
        Session session = factory.getCurrentSession();
        Transaction tx =session.beginTransaction();
        session.save(st);
+       session.save(address);
        tx.commit();
        session.close();
+       System.out.println("Done...");
         
     }
 }
