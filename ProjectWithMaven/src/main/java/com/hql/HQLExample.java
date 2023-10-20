3f09a7c021087dbcf7f03700a6232dc96a71e845
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-20 23:09:47 +0530

    Add files via upload

--- ProjectWithMaven/src/main/java/com/hql/HQLExample.java
+++ ProjectWithMaven/src/main/java/com/hql/HQLExample.java
@@ -1,9 +1,14 @@
 package com.hql;
+
+import java.util.Arrays;
 import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
 import com.tut.ProjectWithMaven.Student;
 
 public class HQLExample {
@@ -18,12 +23,39 @@ public class HQLExample {
 	Query q =s.createQuery(query);
 	q.setParameter("x", "Varansi");
 	q.setParameter("n", "Rahul Singh");
-
+	 
 	List<Student> list = q.list();
 	 for(Student student : list) {
 		 System.out.println(student.getName() + ":"+student.getCerti().getCourse());
 	 }
-	
+	 
+	 System.out.println("______________________________________________________________");
+	 Transaction tx = s.beginTransaction();
+//___________________________Delete____________________________________________________
+//	Query query1 = s.createQuery("delete from  Student s where s.city =: c");
+//	query1.setParameter("c","Patana");
+//	int d = query1.executeUpdate();
+//	System.out.println("Deleted : ");
+//	System.out.println(d);
+// ___________________________Update____________________________________________________
+	 
+//	Query query2 = s.createQuery("update Student set city =: c where name =: n");
+//	query2.setParameter("c", "Patana");
+//	query2.setParameter("n","Bachau");
+//	int u = query2.executeUpdate();
+//	System.out.println(u + " : object updated");
+
+//_______________________________Join___________________________________________________
+	 
+    Query query3 = s.createQuery("select i.id,i.firstName,i.lastName,i.email,t.title from Instructor as i INNER JOIN i.course as t");
+    
+   List<Object[]> list3 = query3.getResultList();
+   for(Object[] arr : list3) {
+	   System.out.println(Arrays.toString(arr));
+   }
+     
+	 
+	tx.commit();
 	s.close();
 	factory.close();
   }
