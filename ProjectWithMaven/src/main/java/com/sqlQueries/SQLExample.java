f5faa56a9f862104cad9d242a7364f41898bbe1a
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-21 19:31:10 +0530

    com/sqlQueries/SQLExample.java
    Executing Native Sql Queries

---
+++ ProjectWithMaven/src/main/java/com/sqlQueries/SQLExample.java
@@ -0,0 +1,33 @@
+package com.sqlQueries;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
+
+public class SQLExample {
+     public static void main(String[] args) {
+		Configuration cnf = new Configuration();
+		cnf.configure("hibernate.cfg.xml");
+		SessionFactory factory = cnf.buildSessionFactory();
+		Session s = factory.openSession();
+		// SQL
+		
+		String q = "Select * from Student";
+		NativeQuery nq = s.createSQLQuery(q);
+		
+		List<Object[]> list = nq.list();
+		
+		for(Object [] student : list) {
+			System.out.println(student[3]+":"+student[4]);
+		}
+		
+		
+		
+		s.close();
+		factory.close();
+	}
+}
