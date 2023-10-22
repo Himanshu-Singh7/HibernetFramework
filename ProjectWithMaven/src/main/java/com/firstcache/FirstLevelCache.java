21d14300f16cce1f22305dd9f54742c1bb5b0617
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-22 15:22:02 +0530

    /firstcache/FirstLevelCache.java
    First Level Chaching

---
+++ ProjectWithMaven/src/main/java/com/firstcache/FirstLevelCache.java
@@ -0,0 +1,28 @@
+package com.firstcache;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.tut.ProjectWithMaven.Student;
+
+public class FirstLevelCache {
+	public static void main(String[] args) {
+		Configuration cnf = new Configuration();
+		cnf.configure("hibernate.cfg.xml");
+		SessionFactory factory = cnf.buildSessionFactory();
+		Session session = factory.openSession();
+
+		// By default enabled
+		Student student = session.get(Student.class, 3);
+		System.out.println(student);
+		System.out.println("Working something");
+
+		Student student1 = session.get(Student.class, 3);
+		System.out.println(student1);
+		System.out.println(session.contains(student1));
+
+		session.close();
+		factory.close();
+	}
+}
